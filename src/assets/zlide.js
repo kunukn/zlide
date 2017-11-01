3900ec4a711cfe62f809fc93bdae047aeff5991c
Kunuk Nykjær <kunukn@gmail.com>
2017-11-01 21:56:34 +0100

    updates

--- src/assets/zlide.js
+++ src/assets/zlide.js
@@ -1,6 +1,7 @@
 const rAF = window.requestAnimationFrame
   ? window.requestAnimationFrame.bind(window)
   : callback => setTimeout(callback, 0);
+const rAF2 = callback => rAF(() => rAF(callback));
 
 const log = console.log.bind(console);
 const TRANSITION_END = 'transitionend';
@@ -30,6 +31,7 @@ function setToCollapsed(props) {
   element.classList.remove('zlide-expanded');
   element.classList.add('zlide-collapsed');
   element.style.maxHeight = '0px';
+
   if (doneCallback) {
     doneCallback({ type: 'collapsed' });
   }
@@ -41,11 +43,11 @@ function setToExpanded(props) {
   const { element, doneCallback } = parseProps(props);
 
   element.style.maxHeight = '';
-  element.style.willChange = '';
   element.classList.remove('zlide-inert');
   element.classList.remove('zlide-collapsed');
   element.classList.remove('zlide-expanding');
   element.classList.add('zlide-expanded');
+
   if (doneCallback) {
     doneCallback({ type: 'expanded' });
   }
@@ -109,20 +111,22 @@ function expand(props) {
     }
   };
 
-  element.style.willChange = 'max-height';
   const elTransitionBackup = element.style.transition;
   element.style.transition = 'max-height 0s !important';
-  element.style.maxHeight = '';
-  const { height } = element[BCR]();
-  // const height = element.scrollHeight;
-  element.style.transition = elTransitionBackup;
-  element.style.maxHeight = '0px';
 
   rAF(() => {
-    element[AEL](TRANSITION_END, transitionEvent);
     /*
       Same level of nested rAF as collapse to synchronize timing of animation.
     */
+
+    // element.style.maxHeight = '';
+    // const { height } = element[BCR]();
+    const height = element.scrollHeight;
+    //element.style.maxHeight = '0px';
+
+    element[AEL](TRANSITION_END, transitionEvent);
+    element.style.transition = elTransitionBackup;
+
     rAF(() => {
       element.style.maxHeight = `${height}px`;
     });
@@ -162,6 +166,7 @@ zlide.setToCollapsed = setToCollapsed;
 zlide.setToExpanded = setToExpanded;
 zlide.applyDefaultStyleSheet = applyDefaultStyleSheet;
 zlide.rAF = rAF;
+zlide.rAF2 = rAF2;
 zlide.qs = qs;
 zlide.qsa = qsa;
 zlide.VERSION = '0.0.9';
